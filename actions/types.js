df8fd6273d0261055f647b9c1b16ed5a52014000
Kingsley <sengking.tan@gmail.com>
2017-09-15 02:56:27 +0800

    export fetch_shot, like_shot, clear_liked_shots

--- actions/types.js
+++ actions/types.js
@@ -1,2 +1,5 @@
 export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
 export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
+export const FETCH_SHOTS = 'fetch_shot';
+export const LIKE_SHOT = 'like_shot';
+export const CLEAR_LIKED_SHOTS = 'clear_liked_shots';
