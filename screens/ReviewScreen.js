9d92957a0a15fbe7f6d3b717bda7b6bbf09f1f70
Kingsley <sengking.tan@gmail.com>
2017-09-15 19:11:21 +0800

    ReviewScreen to show liked designs

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Button, Icon } from 'react-native-elements';
-
+import { View, Text, ScrollView, Linking, Dimensions, Image } from 'react-native';
+import { Button, Card, Icon } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 class ReviewScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
-    title: 'Review',
+    title: 'LIKED Designs',
     tabBarIcon: ({ tintColor }) => {
       return <Icon name='favorite-border' size={30} color={tintColor} />;
     },
@@ -19,13 +19,61 @@ class ReviewScreen extends Component {
       />
   });
 
+  renderLikedShots() {
+  return this.props.likedShots.map(shot => {
+    const {
+      id, title, images, likes_count, views_count, html_url
+    } = shot;
+
+    return (
+      <Card title={title} key={id}>
+          <View style={{ height: 200 }}>
+            <Image
+              style={styles.imageStyle}
+              source={{ uri: images.normal }}
+            />
+          <View style={styles.detailWrapper}>
+            <Text>{likes_count} likes</Text>
+            <Text>{views_count} views</Text>
+          </View>
+          <Button
+            title="Hire Now!"
+            backgroundColor="#FF0080"
+            onPress={() => Linking.openURL(html_url)}
+          />
+        </View>
+      </Card>
+    );
+  });
+}
+
   render() {
     return (
-      <View>
-        <Text>ReviewScreen</Text>
-      </View>
+      <ScrollView>
+        {this.renderLikedShots()}
+      </ScrollView>
     );
   }
 }
 
-export default ReviewScreen;
+const styles = {
+  imageStyle: {
+    flex: 1
+  },
+
+  italics: {
+    fontStyle: 'italic'
+  },
+  detailWrapper: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+};
+
+function mapStateToProps(state) {
+  return { likedShots: state.likedShots };
+}
+
+export default connect(mapStateToProps)(ReviewScreen);
