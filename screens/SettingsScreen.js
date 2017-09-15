8a6edfe3702ec23731d16a032fbdf7f91e15ffc9
Kingsley <sengking.tan@gmail.com>
2017-09-15 14:42:44 +0800

    reset liked shots

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { clearLikedShots } from '../actions';
 
 class SettingsScreen extends Component {
+  static navigationOptions = {
+    header: {
+      style: {
+        marginTop: Platform.OS === 'android' ? 24 : 0
+      }
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>SettingsScreen</Text>
+        <Text>Want to start over again?</Text>
+        <Button
+          title="Reset Liked Shots"
+          large
+          icon={{ name: 'delete-forever' }}
+          backgroundColor="#F44336"
+          onPress={this.props.clearLikedShots}
+        />
       </View>
     );
   }
 }
 
-export default SettingsScreen;
+export default connect(null, { clearLikedShots })(SettingsScreen);
