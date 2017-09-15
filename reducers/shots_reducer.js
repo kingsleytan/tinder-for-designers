16f9ea40cd5bbee506c216d8cc445c1318f94c4e
Kingsley <sengking.tan@gmail.com>
2017-09-15 18:19:08 +0800

    fix bugs on reducer

--- reducers/shots_reducer.js
+++ reducers/shots_reducer.js
@@ -1,14 +1,13 @@
-import _ from 'lodash';
 import {
   FETCH_SHOTS
 } from '../actions/types';
 
-export default function(state = [], action) {
+const INITIAL_STATE = [];
+
+export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case FETCH_SHOTS:
-    return _.uniqBy([
-      action.payload, ...state
-    ], 'id');
+      return action.payload;
     default:
       return state;
   }
