c04fde0e883cf7c92c7f0d768baf8492b5f0c1bb
Kingsley <sengking.tan@gmail.com>
2017-09-15 19:35:09 +0800

    fixed bugs for clear liked shots

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Platform } from 'react-native';
+import { View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { clearLikedShots } from '../actions';
 
 class SettingsScreen extends Component {
-  static navigationOptions = {
-    header: {
-      style: {
-        marginTop: Platform.OS === 'android' ? 24 : 0
-      }
-    }
-  }
-
   render() {
     return (
       <View>
-        <Text>Want to start over again?</Text>
         <Button
           title="Reset Liked Shots"
           large
