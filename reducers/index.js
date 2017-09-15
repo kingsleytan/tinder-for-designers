ac2b56c6f0e240520286fcb80dec92dfcc4d63be
Kingsley <sengking.tan@gmail.com>
2017-09-15 14:43:58 +0800

    import likedShots

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import auth from './auth_reducer';
+import shots from './shots_reducer';
+import likedShots from './likes_reducer';
 
 export default combineReducers({
-  auth
+  auth, shots, likedShots
 });
