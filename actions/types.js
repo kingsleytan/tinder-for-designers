d5cf43180bf7ceaf1b91819aeb2bb26f5b58ae9f
Kingsley <sengking.tan@gmail.com>
2017-09-15 16:02:03 +0800

    fixed typo of 'fetch_shots'

--- actions/types.js
+++ actions/types.js
@@ -1,5 +1,5 @@
 export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
 export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
-export const FETCH_SHOTS = 'fetch_shot';
+export const FETCH_SHOTS = 'fetch_shots';
 export const LIKE_SHOT = 'like_shot';
 export const CLEAR_LIKED_SHOTS = 'clear_liked_shots';
